0da2a706d31c6d4cdd464965d20be19559d1fa39
Brett LeBaron <brett.lebaron@gmail.com>
2019-08-03 21:53:47 -0400

    added limit to scraped articles to 6

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -2,7 +2,10 @@
 $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
+        var articleURL = "https://www.reuters.com"+ data[i].link;
       // Display the apropos information on the page
-      $("#scrape-results").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].summary + "<br />" + data[i].link + "</p>");
+      $("#scrape-results").append("<p data-id='" + data[i]._id + "'>" + data[i].title + 
+      "<button class='save-article button is-info is-medium' data-id='" + data[i]._id + "'><span class='icon'><i class='fa fa-bookmark'></i></span>Save Article</button>"
+      + "<br />" + data[i].summary + "<br />" + articleURL + "</p>" + "<br><br>");
     }
   });
